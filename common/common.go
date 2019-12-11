5c51284d3585989153f90ba99d65e0de9ad2cd42
= <=>
2019-12-11 23:42:59 +0530

    random key

--- common/common.go
+++ common/common.go
@@ -1,10 +1,12 @@
 package common
 
+import "math/rand"
+
 //usernames in both request and response indicate who the message is from
 
 type Response struct {
-	ResTag  string `json:"restag"`
-	Message string `json:"message"`
+	ResTag   string `json:"restag"`
+	Message  string `json:"message"`
 	Username string `json:"username"`
 }
 
@@ -15,7 +17,7 @@ type Request struct {
 	Message  string `json:"message"`
 }
 
-func GenerateRandomKey() string{
+func GenerateRandomKey() string {
 	return RandomString(10)
 }
 
@@ -30,11 +32,11 @@ func RandomString(n int) string {
 }
 
 func NewRequest(reqTag string, username string, pubkey string, message string) *Request {
-	return &Request{ReqTag: reqTag, Username: username, Pubkey:pubkey, Message:message}
+	return &Request{ReqTag: reqTag, Username: username, Pubkey: pubkey, Message: message}
 }
 
 func NewResponse(resTag string, message string, username string) *Response {
-	return &Response{ResTag: resTag, Message:message, Username: username}
+	return &Response{ResTag: resTag, Message: message, Username: username}
 }
 
 func AsymmetricEncryption(key string, message string) string {
@@ -46,27 +48,27 @@ func SymmetricEncryption(key string, message string) string {
 }
 
 func AsymmetricDecryption(key string, message string) string {
-	return "decrypted : "+message
+	return "decrypted : " + message
 }
 
 func SymmetricDecryption(key string, message string) string {
-	return "decrypted : "+message
+	return "decrypted : " + message
 }
 
-const GET_CLIENTS   		= "get_clients"
-const LOGIN 	    		= "login"
-const SIGNUP 	    		= "signup"
-const SELECT_TARGET 		= "selectTarget"
-const TARGET_FAIL   		= "targetFail"
-const TARGET_SET    		= "targetset"
-const SIGNUP_FAILURE    	= "signupfailure"
-const SIGNUP_SUCCESSFUL 	= "signupsuccess"
-const ERROR    				= "error"
-const LOGIN_SUCCESS    		= "login_success"
-const CLIENTS_LIST    		= "clients_list"
-const NONE					= "NONE"
-const CLIENT_MESSAGE 		= "message"
+const GET_CLIENTS = "get_clients"
+const LOGIN = "login"
+const SIGNUP = "signup"
+const SELECT_TARGET = "selectTarget"
+const TARGET_FAIL = "targetFail"
+const TARGET_SET = "targetset"
+const SIGNUP_FAILURE = "signupfailure"
+const SIGNUP_SUCCESSFUL = "signupsuccess"
+const ERROR = "error"
+const LOGIN_SUCCESS = "login_success"
+const CLIENTS_LIST = "clients_list"
+const NONE = "NONE"
+const CLIENT_MESSAGE = "message"
 const CONNECTION_SUCCESSFUL = "connection_successful"
-const SERVER_KEY_EXCHANGE 	= "server_key_exchange"
-const SERVER_KEY_ACK 		= "server_key_ack"
-const TARGET_NOT_SET 		= "target_not_set"
+const SERVER_KEY_EXCHANGE = "server_key_exchange"
+const SERVER_KEY_ACK = "server_key_ack"
+const TARGET_NOT_SET = "target_not_set"
