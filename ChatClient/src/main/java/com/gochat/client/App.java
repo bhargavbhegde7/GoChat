53606c8162f76a5c7bc61379b96400a9a468dc0c
bhegde <bhargavbhegde7@gmail.com>
2020-05-02 23:10:43 +0530

    java client

--- ChatClient/src/main/java/com/gochat/client/App.java
+++ ChatClient/src/main/java/com/gochat/client/App.java
@@ -1,9 +1,31 @@
 package com.gochat.client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 import javax.swing.SwingUtilities;
 
 public class App {
 	public static void main(String[] args) {
+
+		try {
+			Socket socket = new Socket("127.0.0.1", 8080);
+			InputStream input = socket.getInputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+			while(true){
+				String line = reader.readLine();
+				System.out.println(line);
+			}
+
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		SwingUtilities.invokeLater(() -> new App().buildMainApp());
 	}
 
